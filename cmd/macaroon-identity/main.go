e9f2b385abe32f83132d4d6a3441041408244e21
Alberto Donato <alberto.donato@gmail.com>
2017-09-28 13:54:45 -0400

    Move and split binaries
    Signed-off-by: Alberto Donato <alberto.donato@gmail.com>

---
+++ cmd/macaroon-identity/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/albertodonato/macaroon-identity/service"
+)
+
+func main() {
+	endpoint := flag.String("endpoint", "localhost:8080", "service endpoint")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	s := service.NewAuthService(*endpoint, logger)
+	if err := s.Start(); err != nil {
+		panic(err)
+	}
+}
